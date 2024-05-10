4bd3d10d911b6cb9d9ed71596adcbd9de903bcb5
Tuojing huang <a24364042@gmail.com>
2024-05-11 01:34:50 +0800

    Update the pwd_re.py

--- pwd_re.py
+++ pwd_re.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 count = 0
 while count < 3:
+	count = count + 1
 	in_pwd = input('請輸入您的密碼: ')
 	if in_pwd == password:
 		print('登入成功')
 		break
 
 	else:
-		count = count + 1
-		print('密碼錯誤! 還有', (3-count), '次機會')	
+		print('密碼錯誤!') 
+		if (3-count) > 0 :
+			print('還有', (3-count), '次機會')
+		else:
+			print('沒機會嘗試了! 要鎖帳號了啦!')	
